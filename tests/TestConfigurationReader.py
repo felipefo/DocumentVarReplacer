24a1f099c476095b4b3d74fb6df790cfe1d630ac
felipefo <felipefo@users.noreply.github.com>
2022-08-23 13:45:59 -0300

    .
    .

--- tests/TestConfigurationReader.py
+++ tests/TestConfigurationReader.py
@@ -9,7 +9,7 @@ class TesteConfigurationReader(unittest.TestCase):
     def test_variable_reader(self):
         configuration = ConfigurationReader("./TestConfiguration.properties")
         self.assertEqual( "../input/com_coorientador", configuration.get_input_base_path())
-        self.assertEqual("../output/documents", configuration.get_output_base_path())
+        self.assertEqual( "../output/documents", configuration.get_output_base_path())
         self.assertEqual("Bacharelado de Sistemas de Informação", configuration.get_variable_value("c_1"))
 
 if __name__ == '__main__':
