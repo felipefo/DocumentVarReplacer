d4cd77c2e91c55ddcb551372f66de0401de4827d
felipefo <felipefo@users.noreply.github.com>
2022-08-23 15:47:59 -0300

    ,
    ,

--- tests/TestWordReplaceTextWriter.py
+++ tests/TestWordReplaceTextWriter.py
@@ -9,7 +9,7 @@ class TestWordReplaceTextWriter(unittest.TestCase):
     @unittest.expectedFailure
     def test_check_document_type(self):
         mock_configuration = Mock()
-        word_replace = WORDReplaceTextWriter(mock_configuration, "..//teste.odt", "")
+        word_replace = WORDReplaceTextWriter(mock_configuration, ".//teste.odt", "")
         word_replace.check_document_type()
 
     def test_replace_variable_document(self):
@@ -17,10 +17,10 @@ class TestWordReplaceTextWriter(unittest.TestCase):
         mock_configuration = Mock()
         mock_configuration.get_input_variables_keys.return_value = {"c_1": "replace_c_1"}
         mock_configuration.get_variable_value.return_value = "replace_c_1"
-        mock_configuration.output_base_path.return_value = '../'
-        mock_configuration.get_output_base_path.return_value = '../'
+        mock_configuration.output_base_path.return_value = './'
+        mock_configuration.get_output_base_path.return_value = './'
 
-        word_replace = WORDReplaceTextWriter(mock_configuration, "../teste.docx", "..//teste_out.docx")
+        word_replace = WORDReplaceTextWriter(mock_configuration, "teste.docx", ".//teste_out.docx")
 
         word_replace.open_document()
         word_replace.replace_variable_document()
