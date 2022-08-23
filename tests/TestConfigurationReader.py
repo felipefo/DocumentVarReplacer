24555691c60aabd71ec56ec2234fea20174909a1
felipefo <felipefo@users.noreply.github.com>
2022-08-23 14:02:06 -0300

    .
    .

--- tests/TestConfigurationReader.py
+++ tests/TestConfigurationReader.py
@@ -1,3 +1,4 @@
+
 import unittest
 
 from src.file.ConfigurationReader import ConfigurationReader
@@ -6,12 +7,10 @@ from src.file.ConfigurationReader import ConfigurationReader
 class TesteConfigurationReader(unittest.TestCase):
 
     def test_variable_reader(self):
-        print('ok')
-        #    configuration = ConfigurationReader("../tests/TestConfiguration.properties")
-    #    self.assertEqual( "../input/com_coorientador", configuration.get_input_base_path())
-    #    self.assertEqual( "../output/documents", configuration.get_output_base_path())
-    #    self.assertEqual("Bacharelado de Sistemas de Informação", configuration.get_variable_value("c_1"))
-
+        configuration = ConfigurationReader("../tests/TestConfiguration.properties")
+        self.assertEqual( "../input/com_coorientador", configuration.get_input_base_path())
+        self.assertEqual( "../output/documents", configuration.get_output_base_path())
+        self.assertEqual("Bacharelado de Sistemas de Informação", configuration.get_variable_value("c_1"))
 
 if __name__ == '__main__':
     unittest.main()
