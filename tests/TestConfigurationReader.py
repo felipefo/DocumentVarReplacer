aa4893f95b62e40d605590e1a4caa84f24c7dee3
felipefo <felipefo@users.noreply.github.com>
2022-08-24 08:12:15 -0300

    Fixing working locally also
    Fixing working locally also

--- tests/TestConfigurationReader.py
+++ tests/TestConfigurationReader.py
@@ -6,7 +6,7 @@ from src.file.ConfigurationReader import ConfigurationReader
 class TesteConfigurationReader(unittest.TestCase):
 
     def test_variable_reader(self):
-        configuration = ConfigurationReader("./tests/TestConfiguration.properties")
+        configuration = ConfigurationReader("../tests/TestConfiguration.properties")
         self.assertEqual("./tests/", configuration.get_input_base_path())
         self.assertEqual("./tests/", configuration.get_output_base_path())
         self.assertEqual("Bacharelado de Sistemas de Informacao", configuration.get_variable_value("c_1"))
