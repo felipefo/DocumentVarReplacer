14b2ade2868c5158c350f83a1e96c94ece0d8f18
felipefo <felipefo@users.noreply.github.com>
2022-08-24 08:00:09 -0300

    Removing acent from the code
    Removing acent from the code

--- tests/TestConfigurationReader.py
+++ tests/TestConfigurationReader.py
@@ -9,7 +9,7 @@ class TesteConfigurationReader(unittest.TestCase):
         configuration = ConfigurationReader("./tests/TestConfiguration.properties")
         self.assertEqual("../input/com_coorientador", configuration.get_input_base_path())
         self.assertEqual("../output/documents", configuration.get_output_base_path())
-        self.assertEqual("Bacharelado de Sistemas de Informação", configuration.get_variable_value("c_1"))
+        self.assertEqual("Bacharelado de Sistemas de Informacao", configuration.get_variable_value("c_1"))
 
 if __name__ == '__main__':
     unittest.main()
