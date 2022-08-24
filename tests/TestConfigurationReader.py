4d65d1ff4b9f91971125a77479555293b975c3da
felipefo <felipefo@users.noreply.github.com>
2022-08-24 08:07:41 -0300

    Fixing test one

--- tests/TestConfigurationReader.py
+++ tests/TestConfigurationReader.py
@@ -7,8 +7,8 @@ class TesteConfigurationReader(unittest.TestCase):
 
     def test_variable_reader(self):
         configuration = ConfigurationReader("./tests/TestConfiguration.properties")
-        self.assertEqual("./tests", configuration.get_input_base_path())
-        self.assertEqual("./tests", configuration.get_output_base_path())
+        self.assertEqual("./tests/", configuration.get_input_base_path())
+        self.assertEqual("./tests/", configuration.get_output_base_path())
         self.assertEqual("Bacharelado de Sistemas de Informacao", configuration.get_variable_value("c_1"))
 
 if __name__ == '__main__':
