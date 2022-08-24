e91e881d9b3e2b51423c29548e3b6ecdaa8f0b70
felipefo <felipefo@users.noreply.github.com>
2022-08-24 07:44:57 -0300

    fixing relative input files names of test case

--- tests/TestConfigurationReader.py
+++ tests/TestConfigurationReader.py
@@ -6,7 +6,7 @@ from src.file.ConfigurationReader import ConfigurationReader
 class TesteConfigurationReader(unittest.TestCase):
 
     def test_variable_reader(self):
-        configuration = ConfigurationReader("TestConfiguration.properties")
+        configuration = ConfigurationReader("./TestConfiguration.properties")
         self.assertEqual("../input/com_coorientador", configuration.get_input_base_path())
         self.assertEqual("../output/documents", configuration.get_output_base_path())
         self.assertEqual("Bacharelado de Sistemas de Informação", configuration.get_variable_value("c_1"))
